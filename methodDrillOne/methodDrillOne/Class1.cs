84d94982e254d9147936cbdff636650383c21d20
tonythach <tonythach@gmail.com>
2018-04-26 08:33:40 -0700

    files added

---
+++ methodDrillOne/methodDrillOne/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodDrillOne
+{
+    class Table
+
+    {
+        public int Add(int number)
+        {
+            int result = number + 150;
+            return result;
+        }
+        public int Subtract(int number)
+        {
+            int difference = 1000 - number;
+            return difference;
+        }
+        public int Multiply(int number)
+        {
+            int product = number * 6;
+            return product;
+        }
+    }
+}
---
+++ methodDrillOne/methodDrillOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodDrillOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Good afternoon! Please enter a numeric value!");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+
+            Table table = new Table();
+            int result = table.Add(userValue);
+            Console.WriteLine("Your number plus 150 is " + result);
+
+            int difference = table.Subtract(userValue);
+            Console.WriteLine("Your number subtracted from 1000 is " + difference);
+
+            int product = table.Multiply(userValue);
+            Console.WriteLine("Your number multiplied by 6 is " + product);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
